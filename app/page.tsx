b58730a20b84aea6b805df74eea329d6586891a0
santhosh12133 <your-github-santhoshkumar12133@gamil.com>
2026-02-17 00:48:29 +0530

    Initial Next.js App Router setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800">
+        Smart Bookmark App 🚀
+      </h1>
+    </main>
+  );
+}
